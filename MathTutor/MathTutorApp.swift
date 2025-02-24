be3b6e1202e317c9832090bf2bd4afeaf61baeb4
4OH4Loop <semperchuck@gmail.com>
2025-02-24 18:13:10 -0500

    Initial Commit

---
+++ MathTutor/MathTutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathTutorApp.swift
+//  MathTutor
+//
+//  Created by Carolyn Ballinger on 2/24/25.
+//
+
+import SwiftUI
+
+@main
+struct MathTutorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

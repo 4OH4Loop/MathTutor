1edae8af0a7c44748ae929579b54516e3c23c2b2
4OH4Loop <semperchuck@gmail.com>
2025-02-24 18:46:53 -0500

    Midterm Problem 2

--- MathTutor/ContentView.swift
+++ MathTutor/ContentView.swift
@@ -6,14 +6,51 @@
 //
 
 import SwiftUI
+import AVFAudio
 
 struct ContentView: View {
+    @State private var firstNumber = Int.random(in: 1...10)
+    @State private var secondNumber = Int.random(in: 1...10)
+    @State private var guessedNumber = ""
+    @State private var answer = ""
+    @State private var audioPlayer: AVAudioPlayer!
+    let emojis = ["🍕", "🍎", "🍏", "🐵", "👽", "🧠", "🧜", "🧙", "🥷", "🐶", "🐹", "🐣", "🦄", "🐝", "🦉", "🦋",
+                  "🦖", "🐙", "🦞", "🐟", "🦔", "🐲", "🌻", "🌍", "🌈", "🍔", "🌮", "🍦", "🍩", "🍪"]
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+// TODO: Number of emojis must equal number
+//            String(emojis[firstNumber])
+//            String(emojis[secondNumber])
+//            Image(emojis[firstNumber])
+//                .resizable()
+//                .scalable()
+            Spacer()
+            
+            Text("\(firstNumber) + \(secondNumber) = ")
+                .font(.largeTitle)
+            TextField("", text: $guessedNumber)
+                .font(.largeTitle)
+                .textFieldStyle(.roundedBorder)
+                .frame(width: 60)
+                .overlay {
+                    RoundedRectangle(cornerRadius: 5)
+                        .stroke(Color.gray, lineWidth: 2)
+                }
+                .keyboardType(.numberPad)
+            
+            Button("Guess") {
+                guessedNumber()
+                    .buttonStyle[.borderedProminent]
+                    .disabled(guessedNumber.isEmpty)
+            }
+            
+//            func guessANumber() {
+//                textFieldIsFocused = false
+//                
+            }
+                
+                
         }
         .padding()
     }
